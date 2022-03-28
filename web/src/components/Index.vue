dd16591d9b2a68f0537105d85320090d6d7f5f5d
np1139059565 <1139059565@qq.com>
2022-03-28 19:56:59 +0800

    add

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -114,7 +114,7 @@ export default {
       var isone=false
       if(inputStr==null){
         isone=true
-        inputStr=prompt("打开新扫描(1代表全部;搜索多种机型时,使用\\n换行)", pinfo.search_str)
+        inputStr=prompt("打开新扫描(1代表全部(写了1就不能再写其他的数字了),使用~连接数字;搜索多种机型时,使用\\n换行)", pinfo.search_str)
       }
       //open scan..
       if(inputStr!=null){
